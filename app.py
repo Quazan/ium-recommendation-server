de59e9899c21e9a8e47182ad24774040a4f4484d
Quazan <alexer98@gmail.com>
2020-06-20 20:43:51 +0200

    added endpoints

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
 from flask import Flask, jsonify, request
-
+from collaborativeFiltering import CollaborativeFilteringModel
+from contentBased import ContentBasedModel
 
 app = Flask(__name__)
 
+content = ContentBasedModel()
+collaborative = CollaborativeFilteringModel()
 
-@app.route('/app')
+
+@app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/predict')
+def predict():
+    userId = int(request.args.get('userId'))
+    productId = int(request.args.get('productId'))
+
+    if userId % 2 == 0:
+        pred = content.predict(productId)
+        mod = 'content based'
+    else:
+        pred = collaborative.predict(userId)
+        mod = 'user based'
+
+    return jsonify(user=userId,
+                   product=productId,
+                   prediction=pred,
+                   model=mod)
+
+
+@app.route('/collaborative')
+def predictCollaborative():
+    userId = int(request.args.get('userId'))
+    return jsonify(user=userId,
+                   prediction=collaborative.predict(userId))
+
+
+@app.route('/content')
+def predictionContent():
+    productId = int(request.args.get('productId'))
+    return jsonify(product=productId,
+                   prediction=content.predict(productId))
 
 
 if __name__ == '__main__':
